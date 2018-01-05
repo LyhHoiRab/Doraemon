576061710fcb29e812ec9c11245569f8448f3465
lihaihua <lihaihua@email.com>
2018-01-05 18:40:44 +0800

    测试类

--- src/main/java/org/wah/doraemon/utils/DateUtils.java
+++ src/main/java/org/wah/doraemon/utils/DateUtils.java
@@ -3,6 +3,8 @@ package org.wah.doraemon.utils;
 import org.apache.commons.lang3.StringUtils;
 import org.wah.doraemon.security.exception.UtilsException;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -15,6 +17,16 @@ public class DateUtils{
 
     //默认时间格式化模板
     private static final String DEFAULT_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    //毫秒对秒
+    private static final long MILLISECOND_SECOND = 1000;
+    //毫秒对分钟
+    private static final long MILLISECOND_MINUTE = MILLISECOND_SECOND * 60;
+    //毫秒对小时
+    private static final long MILLISECOND_HOUR = MILLISECOND_MINUTE * 60;
+    //毫秒对天
+    private static final long MILLISECOND_DAY = MILLISECOND_HOUR * 24;
+    //毫秒对周
+    private static final long MILLISECOND_WEEK = MILLISECOND_DAY * 7;
 
     public DateUtils(){
 
@@ -203,5 +215,340 @@ public class DateUtils{
         cal_second.setTime(second);
 
         return cal_first.get(Calendar.ERA) == cal_second.get(Calendar.ERA);
+    }
+
+    /**
+     * 当天凌晨
+     * 00:00:00.0
+     */
+    public static Date firstTimeOfDay(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 当天深夜
+     * 23:59:59.999
+     */
+    public static Date lastTimeOfDay(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在周的第一天
+     */
+    public static Date firstDayOfWeek(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getActualMinimum(Calendar.DAY_OF_WEEK));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在周的最后一天
+     */
+    public static Date lastDayOfWeek(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getActualMaximum(Calendar.DAY_OF_WEEK));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在月的第一天
+     */
+    public static Date firstDayOfMonth(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在月的最后一天
+     */
+    public static Date lastDayOfMonth(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在年的一天
+     */
+    public static Date firstDayOfYear(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.getActualMinimum(Calendar.DAY_OF_YEAR));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 查询日期所在年的最后一天
+     */
+    public static Date lastDayOfYear(Date date){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.getActualMaximum(Calendar.DAY_OF_YEAR));
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 日期增量算法
+     */
+    public static Date add(Date date, int field, int amount){
+        if(date == null){
+            throw new UtilsException("日期对象不能为空");
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(field, amount);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 追加毫秒数
+     */
+    public static Date addMilliseconds(Date date, int amount){
+        return add(date, Calendar.MILLISECOND, amount);
+    }
+
+    /**
+     * 追加秒数
+     */
+    public static Date addSeconds(Date date, int amount){
+        return add(date, Calendar.SECOND, amount);
+    }
+
+    /**
+     * 追加分钟数
+     */
+    public static Date addMinutes(Date date, int amount){
+        return add(date, Calendar.MINUTE, amount);
+    }
+
+    /**
+     * 追加小时数
+     */
+    public static Date addHours(Date date, int amount){
+        return add(date, Calendar.HOUR, amount);
+    }
+
+    /**
+     * 追加天数
+     */
+    public static Date addDays(Date date, int amount){
+        return add(date, Calendar.DAY_OF_YEAR, amount);
+    }
+
+    /**
+     * 追加周数
+     */
+    public static Date addWeeks(Date date, int amount){
+        return add(date, Calendar.WEEK_OF_YEAR, amount);
+    }
+
+    /**
+     * 追加月数
+     */
+    public static Date addMonths(Date date, int amount){
+        return add(date, Calendar.MONTH, amount);
+    }
+
+    /**
+     * 追加年数
+     */
+    public static Date addYears(Date date, int amount){
+        return add(date, Calendar.YEAR, amount);
+    }
+
+    /**
+     * 毫秒差
+     */
+    public static long differenceByMilliseconds(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        return Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+    }
+
+    /**
+     * 秒数差
+     */
+    public static long differenceBySeconds(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        long milliseconds = Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+
+        return milliseconds / MILLISECOND_SECOND;
+    }
+
+    /**
+     * 分钟差
+     */
+    public static long differenceByMinutes(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        long milliseconds = Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+
+        return milliseconds / MILLISECOND_MINUTE;
+    }
+
+    /**
+     * 小时差
+     */
+    public static long differenceByHours(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        long milliseconds = Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+
+        return milliseconds / MILLISECOND_HOUR;
+    }
+
+    /**
+     * 天数差
+     */
+    public static long differenceByDays(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        long milliseconds = Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+
+        return milliseconds / MILLISECOND_DAY;
+    }
+
+    /**
+     * 周数差
+     */
+    public static long differenceByWeeks(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        long milliseconds = Math.abs(cal_first.getTimeInMillis() - cal_second.getTimeInMillis());
+
+        return milliseconds / MILLISECOND_WEEK;
+    }
+
+    /**
+     * 月数差
+     */
+    public static long differenceByMonths(Date first, Date second){
+        return 0;
+    }
+
+    /**
+     * 年数差
+     */
+    public static long differenceByYears(Date first, Date second){
+        if(first == null || second == null){
+            throw new UtilsException("比较的日期不能为空");
+        }
+
+        Calendar cal_first = Calendar.getInstance();
+        cal_first.setTime(first);
+
+        Calendar cal_second = Calendar.getInstance();
+        cal_second.setTime(second);
+
+        return Math.abs(cal_first.get(Calendar.YEAR) - cal_second.get(Calendar.YEAR));
     }
 }
--- src/main/test/utils/DateUtilsTest.java
+++ src/main/test/utils/DateUtilsTest.java
@@ -7,7 +7,13 @@ import java.util.Date;
 public class DateUtilsTest{
 
     public static void main(String[] args){
-        parse();
+//        parse();
+//        dayOfWeek();
+//        dayOfMonth();
+//        dayOfYear();
+//        add();
+
+        difference();
     }
 
     public static void parse(){
@@ -16,5 +22,48 @@ public class DateUtilsTest{
 
         Date result = DateUtils.parse(date, pattern, true);
         System.out.println(result);
+    }
+
+    public static void dayOfWeek(){
+        System.out.println(DateUtils.firstDayOfWeek(new Date()));
+        System.out.println(DateUtils.lastDayOfWeek(new Date()));
+    }
+
+    public static void dayOfMonth(){
+        System.out.println(DateUtils.firstDayOfMonth(new Date()));
+        System.out.println(DateUtils.lastDayOfMonth(new Date()));
+    }
+
+    public static void dayOfYear(){
+        System.out.println(DateUtils.firstDayOfYear(new Date()));
+        System.out.println(DateUtils.lastDayOfYear(new Date()));
+    }
+
+    public static void add(){
+        Date date = new Date();
+
+        System.out.println(DateUtils.addMilliseconds(date, 1));
+        System.out.println(DateUtils.addSeconds(date, 1));
+        System.out.println(DateUtils.addMinutes(date, 1));
+        System.out.println(DateUtils.addHours(date, 1));
+        System.out.println(DateUtils.addDays(date, 1));
+        System.out.println(DateUtils.addWeeks(date, 4));
+        System.out.println(DateUtils.addMonths(date, 1));
+        System.out.println(DateUtils.addYears(date, 1));
+    }
+
+    public static void difference(){
+        Date date = new Date();
+        Date first = date;
+        Date second = DateUtils.addDays(first, 14);
+
+        System.out.println(DateUtils.differenceByMilliseconds(first, second));
+        System.out.println(DateUtils.differenceBySeconds(first, second));
+        System.out.println(DateUtils.differenceByMinutes(first, second));
+        System.out.println(DateUtils.differenceByHours(first, second));
+        System.out.println(DateUtils.differenceByDays(first, second));
+        System.out.println(DateUtils.differenceByWeeks(first, second));
+        System.out.println(DateUtils.differenceByMonths(first, second));
+        System.out.println(DateUtils.differenceByYears(first, second));
     }
 }
